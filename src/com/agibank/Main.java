ca787fbfaf0520a7ecd9a43eb9fa402d5c744570
matheus.rocco <roccomatheus0@gmail.com>
2025-02-18 09:37:40 -0300

    add subtraction method

--- src/com/agibank/Main.java
+++ src/com/agibank/Main.java
@@ -6,7 +6,12 @@ public class Main {
         return a + b;
     }
 
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Resultado: " + somar(3,5));
+        System.out.println("Resultado: " + subtraction(3,5));
     }
 }
