1f3dc829ad7d80cc011658e8f26249b2335b7b56
matheus.rocco <roccomatheus0@gmail.com>
2025-02-18 09:15:36 -0300

    project created

---
+++ src/com/agibank/Main.java
@@ -0,0 +1,12 @@
+package com.agibank;
+
+public class Main {
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Resultado: " + somar(3,5));
+    }
+}
